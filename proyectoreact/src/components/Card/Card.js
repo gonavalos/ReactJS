1accb74a554345243b72ac8c6cbb00f0d0f6f76f
gonavalos <gonn.avalos@icloud.com>
2022-05-24 16:13:05 -0300

    modificacion

--- proyectoreact/src/components/Card/Card.js
+++ proyectoreact/src/components/Card/Card.js
@@ -8,9 +8,9 @@ const Card = () => {
     <div class="product">
 
       <img class="product__image" src="./img/img-eccomerce/tomahawk2.jpg"></img>
-      <h1 class="product__title">Tomahawk</h1>
+      <h1 class="product__title">Tomahawk Steak</h1>
       <hr />
-      <p>The Nike Roshe One Print Men's Shoe offers breathability, lightweight cushioning and bold style with an injected unit midsole and colorful mesh upper. </p>
+      <p>Es un filete grueso con costilla y una presentación impresionante, que poco a poco se ha ido ganando su estatus dentro de la carne de vaca. La gran infiltración de grasa intramuscular (marmoleado) e intermuscular junto con el hueso de la costilla le confiere un gran sabor y una jugosidad excepcional.</p>
       <ItemCount />
     </div>
 
--- proyectoreact/src/components/ItemCount/ItemCount.js
+++ proyectoreact/src/components/ItemCount/ItemCount.js
@@ -4,20 +4,22 @@ const CountFunction = () => {
     const [count, setCount] = useState(0)
 
     const decrement = () => {
-        
+            
             setCount((count) => count - 500)
     }
 
     const increment = () => {
         setCount(count + 500)
     }
+    
+    
 
 
 
     return(
         <div className="product__btn btn">
 
-            <span onClick={decrement}>-</span>
+            <span onClick={count > 0 ? decrement : ''} >-</span>
         
 
             <a href="#"> {count} Grs. </a>
